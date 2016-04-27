c4c4e9c7a53b1fd1831bd6ce26d7692ee2faf2d1
xiasuochen <xiasuochen@126.com>
2016-04-27 11:18:17 +0800

    realize the simple GUI

---
+++ win32/resource.h
@@ -0,0 +1,33 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define DLG_MAIN                                100
+#define IDC_BUTTON2                             40000
+#define IDC_BUTTON1                             40001
+#define IDC_BUTTON3                             40002
+#define IDC_RADIO1                              40003
+#define IDC_RADIO2                              40004
+#define IDC_RADIO3                              40005
+#define IDC_RADIO4                              40006
+#define IDC_RADIO5                              40007
+#define IDC_RADIO6                              40008
+#define IDC_RADIO7                              40009
+#define IDC_RADIO8                              40010
+#define IDC_RADIOVERTICAL                       40011
+#define IDC_RADIO9                              40012
+#define IDC_RADIO10                             40013
+#define IDC_RADIO11                             40014
+#define IDC_RADIO12                             40015
+#define IDC_RADIOHORIZONTAL                     40016
+#define IDC_RADIO13                             40017
+#define IDC_RADIO14                             40018
+#define IDC_RADIO15                             40019
+#define IDC_RADIO16                             40020
+#define IDC_RADIO17                             40021
+#define IDC_RADIO18                             40022
+#define IDC_BUTTONTEST                          40023
+#define IDC_EDITBALANCE                         40024
+#define IDC_EDITALIGNMENT                       40025
+#define IDC_EDITCOST                            40026
+#define IDC_BUTTONSTART                         40027
